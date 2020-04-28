f141c540c8e73ca08a013f742a958bf49b18be9d
Thamaltw97 <thamal1997@gmail.com>
2020-04-29 03:17:30 +0530

    Updated server.js (Server and MongoDB)

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Express Middlewares
+app.use(express.json());
+app.use(cors());
+
+//DB Config   //If cloning from git, include mongoURI in './config/keys'
+const uri = require('./config/keys').mongoURI;
+
+//Connect to MongoDB
+mongoose
+    .connect(uri,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB database connection established successfully"))
+    .catch(err => console.log("ERROR: " + err));
+
+// Start server
+app.listen(port, () => {
+    console.log("Server is running on port: " + port);
+});
