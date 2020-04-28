2ee70e2284e8e6d19d2f4be18b475397e2e1ef86
Thamaltw97 <thamal1997@gmail.com>
2020-04-29 03:10:22 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+    );
+  }
+
+}
+
+export default App;
